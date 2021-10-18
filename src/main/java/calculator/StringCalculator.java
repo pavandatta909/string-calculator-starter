e0209f1e22703b82d16f39e083c9583735d4f155
Pavan Datta <68382112+pavandatta909@users.noreply.github.com>
2021-10-18 09:47:24 +0530

    complete string calculator with add method

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,48 @@
-package calculator;
+public class StringCalculator {
 
-class StringCalculator {
+	public static int add(String input) {
+		if (input.equals("")) {
+			return 0;
+		} else {
+			String delimiter = ",";
+			if (input.matches("//(.*)\n(.*)")) {
+				delimiter = Character.toString(input.charAt(2));
+				input = input.substring(4);
+			}
 
-    public int add(String input) {
-        return 0;
+			String numList[] = splitNumbers(input, delimiter + "|\n");
+			return sum(numList);
+		}
+	}
+
+	private static int toInt(String number){
+		return Integer.parseInt(number);
+	}
+
+	private static String[] splitNumbers(String numbers, String divider){
+	    return numbers.split(divider);
+	}
+
+	private static int sum(String[] numbers){
+ 	    int total = 0;
+ 	    String negString = "";
+
+        for(String number : numbers){
+        	if(toInt(number) < 0){
+        		if(negString.equals(""))
+        			negString = number;
+        		else
+        			negString += ("," + number);
+        	}
+        	if(toInt(number) < 1000)
+		    	total += toInt(number);
+		}
+
+		if(!negString.equals("")){
+			throw new IllegalArgumentException("Negatives not allowed: " + negString);
+		}
+
+		return total;
     }
-
+	
 }
